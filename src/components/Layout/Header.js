74ae62e09e75e8927ae191898d9796692aad2040
IdrisGuloyev <idrisguloyev@gmail.com>
2022-09-09 21:53:10 +0600

    Добавление Компонентов для Отображения Блюд

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import sushiImage from "../../assets/sushi.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>Японская Кухня</h1>
-        <button>Корзина</button>
+        <HeaderCartButton />
       </header>
-      <div className={styles['main-image']}>
+      <div className={styles["main-image"]}>
         <img src={sushiImage} alt="Блюда японской кухни" />
       </div>
     </React.Fragment>
