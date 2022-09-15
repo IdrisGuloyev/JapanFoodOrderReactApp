a138653b3b0f5271ca53e1fe6d1e443d04b01940
IdrisGuloyev <idrisguloyev@gmail.com>
2022-09-15 20:02:49 +0600

    AddedRemoveElementsLogic

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,5 +1,4 @@
 import { useReducer } from "react";
-
 import CartContext from "./cart-context";
 
 const defaultCartState = {
@@ -41,6 +40,33 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.id;
+    });
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (action.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
@@ -60,7 +86,7 @@ const CartContextProvider = (props) => {
   const removeItemHandler = (id) => {
     dispatchCartAction({
       type: "REMOVE_ITEM",
-      item: id,
+      id: id,
     });
   };
 
